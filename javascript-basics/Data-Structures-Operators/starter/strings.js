1117325ad99dce2dd797be137edf11d12ea2ecf3
Shruti <dev.shruti27@gmail.com>
2025-05-16 17:02:48 +0200

    final challenge strings

--- javascript-basics/Data-Structures-Operators/starter/strings.js
+++ javascript-basics/Data-Structures-Operators/starter/strings.js
@@ -2,9 +2,6 @@
 
 // Working with strings : 1
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -237,3 +234,61 @@ const planesInLine = function (n) {
 planesInLine(5); // There are 5 planes in line ✈️✈️✈️✈️✈️
 planesInLine(3); // There are 3 planes in line ✈️✈️✈️
 planesInLine(12); // There are 12 planes in line ✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️✈️
+
+// Strings Method Challenge
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25 +_Arrival;bru0943384722;fao93766109;11:45 +_Delayed_Arrival;hel7439299980;fao93766109;12:05 +_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴Delayed Departure from FAO to TXL (11h25)
+//..............Arrival from BRU to FAO (11h45)
+// ....🔴Delayed Arrival from HEL to FAO (12h05)
+// .............Departure from FAO to LIS (12h30)
+
+const separateFlightDetails = flights.split('+');
+console.log(separateFlightDetails);
+
+const newFlightTextArr = [];
+const allFlightsTransformText = function () {
+  for (let flight of separateFlightDetails) {
+    flight = flight.replaceAll('_', ' ').replaceAll(':', 'h').trim().split(';');
+    console.log(flight);
+
+    const arrStr1 = `from ${flight[1].slice(0, 3).toUpperCase()}`;
+
+    const arrStr2 = `to ${flight[2].slice(0, 3).toUpperCase()}`;
+    const newStr =
+      flight[1].replace(flight[1], arrStr1) +
+      ' ' +
+      flight[2].replace(flight[2], arrStr2);
+
+    newFlightTextArr.push(
+      `${flight[0].startsWith('Delayed') ? '🔴' : ''} ${flight[0]} ${newStr} (${
+        flight[3]
+      })`.padStart(50)
+    );
+  }
+  console.log(newFlightTextArr);
+
+  for (const text of newFlightTextArr) {
+    console.log(text);
+  }
+};
+allFlightsTransformText();
+
+// Solution by Jonas
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
+
+  console.log(output);
+}
